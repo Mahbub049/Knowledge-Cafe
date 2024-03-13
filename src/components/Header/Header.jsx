7ffdf1198be6709db0bac6248af24d0e53b890d4
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-03-13 15:39:33 +0600

    code pushed:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-            <h1 className="text-4xl ">Knowledge Cafe</h1>
-        </div>
+        <header className='mx-auto container mb-[32px]'>
+            <div className="mt-[50px] flex justify-between items-center">
+                <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+                <img className='w-[60px] mb-[32px]' src={profile} alt="" />
+            </div>
+            <hr />
+        </header>
     );
 };
 
